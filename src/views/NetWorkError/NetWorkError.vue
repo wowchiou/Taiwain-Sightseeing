eb42f9d4670c683a5df61cf8b47db568c7b6375c
Aaron Chiou <wowchiou@gmail.com>
2022-03-07 15:22:12 +0800

    update

--- src/views/NetWorkError/NetWorkError.vue
+++ src/views/NetWorkError/NetWorkError.vue
@@ -3,20 +3,14 @@
     <div class="wrap">
       <h1>Network Error</h1>
       <p>抱歉！網路連結異常，請稍後再試！</p>
-      <p>
-        <AppLink :to="{ name: 'home' }" replace>返回首頁</AppLink>繼續瀏覽。
-      </p>
+      <p><a href="/" target="_self">返回首頁</a>繼續瀏覽。</p>
     </div>
   </div>
 </template>
 
 <script>
-import AppLink from '@/components/AppLink';
-
 export default {
-  components: {
-    AppLink,
-  },
+  components: {},
 };
 </script>
 
