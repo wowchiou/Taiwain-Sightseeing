258179c2aca723ed3f9bb18595a30cc9c356ef53
Aaron Chiou <wowchiou@gmail.com>
2022-03-07 15:33:14 +0800

    update

--- src/views/NetWorkError/NetWorkError.vue
+++ src/views/NetWorkError/NetWorkError.vue
@@ -3,14 +3,20 @@
     <div class="wrap">
       <h1>Network Error</h1>
       <p>抱歉！網路連結異常，請稍後再試！</p>
-      <p><a href="/" target="_self">返回首頁</a>繼續瀏覽。</p>
+      <p><a :href="baseUrl" target="_self">返回首頁</a>繼續瀏覽。</p>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  components: {},
+  computed: {
+    baseUrl() {
+      return process.env.NODE_ENV === 'production'
+        ? '/Taiwain-Sightseeing/'
+        : '/';
+    },
+  },
 };
 </script>
 
